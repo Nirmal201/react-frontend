6a4366bc607ca80926a0a14987a30f385e7098c9
Nirmal201 <ninu201@gmail.com>
2020-03-16 00:02:43 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Pet name="Luna" animal="Dog" breed="Mixed"></Pet>
+      <Pet name="Jira" animal="pepera" breed="Masala"></Pet>
+      <Pet name="Meetha" animal="salt" breed="lol"></Pet>
+    </div>
+  );
+};
+
+render(React.createElement(App), document.getElementById("root"));
